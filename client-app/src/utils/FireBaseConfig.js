cb8c3a4bb6ba9a36e9edc25de9cf0d37247065ac
Christapher Antony <christapher012@gmail.com>
2023-04-05 00:03:20 +0530

    google auth config set up

---
+++ client-app/src/utils/FireBaseConfig.js
@@ -0,0 +1,26 @@
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIRE_BASE_API_KEY,
+  authDomain: "otp-verification-a9e0a.firebaseapp.com",
+  projectId: "otp-verification-a9e0a",
+  storageBucket: "otp-verification-a9e0a.appspot.com",
+  messagingSenderId: "1044039273691",
+  appId: "1:1044039273691:web:43765717f747e1132890fe",
+  measurementId: "G-F9QELD22MY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider()
+const auth = getAuth(app);
+
+export { provider, auth }
